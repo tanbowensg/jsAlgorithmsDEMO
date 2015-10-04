9b0516c56b1c5e01baf08a72612e25d1180ce945
root <root@localhost.localdomain>
2015-10-04 22:47:18 +0800

    添加了归并排序和快速排序

--- js.js
+++ js.js
@@ -11,7 +11,7 @@ var List = function(l, max, isInt) {
     return array
 }
 
-var theArray = List(64000, 1000, true)
+var theArray = List(100000, 1000, true)
 
 function less(a, b) {
     if (a < b) {
@@ -85,7 +85,7 @@ howLongDoseItTake = function(func, obj) {
     var d1 = new Date()
     startTime = d1.getTime();
 
-    func.apply(obj)
+    func.apply(obj,obj)
 
     var d2 = new Date()
     endTime = d2.getTime();
@@ -203,4 +203,191 @@ Array.prototype.shellSort = function() {
     console.log(this)
     console.log('理论上应进行约' + comparet + '次比较和' + exchanget + '次交换')
     console.log('实际上进行了共' + compare + '次比较和' + exchange + '次交换')
+}
+
+Array.prototype.mergeSort = function() {
+
+    function sort(array) {
+
+        var length = array.length,
+            mid    = Math.floor(length * 0.5),
+            left   = array.slice(0, mid),
+            right  = array.slice(mid, length);
+
+        if(length === 1) {
+          return array;
+        }
+
+        return merge(sort(left), sort(right));
+
+    }
+
+    function merge(left, right) {
+        var result = []
+        for (var li=0,ri=0,i=0;i<left.length+right.length;i++){
+            if (li < left.length && ri < right.length) {
+                if (less(left[li], right[ri])) {
+                    result[i]=left[li]
+                    li++
+                    continue
+                }
+                else {
+                    result[i]=right[ri]
+                    ri++
+                    continue
+                }
+            }
+            if (li===left.length) {
+                result[i]=right[ri]
+                ri++
+                continue
+            }
+            else {
+                result[i]=left[li]
+                li++
+                continue
+            }
+        }
+        virtualize(result, 0)
+        return result
+    }
+    var newArray=sort(this)
+    return newArray
+}
+
+// Array.prototype.mergeSort2 = function() {
+
+//   function sort(array) {
+
+//     var length = array.length,
+//         mid    = Math.floor(length * 0.5),
+//         left   = array.slice(0, mid),
+//         right  = array.slice(mid, length);
+
+//     if(length === 1) {
+//       return array;
+//     }
+
+//     return merge(sort(left), sort(right));
+
+//   }
+
+//     function merge(left, right) {
+//         var result = []
+//         while (left.length || right.length) {
+//             if (left[0] < right[0]) {
+//                 result.push(left.shift());
+//                 continue
+//             }
+//             if (left[0] >= right[0]) {
+//                 result.push(right.shift());
+//                 continue
+//             }
+//             if (left.length === 0) {
+//                 result.push(right.shift());
+//                 continue
+//             }
+//             if (right.length === 0) {
+//                 result.push(left.shift());
+//                 continue
+//             }
+//         }
+
+//         return result
+//     }
+
+//     var newArray=sort(this)
+//     this.splice(0,this.length,newArray)
+//     console.log(this)
+// }
+
+Array.prototype.quickSort=function(){
+
+  /**
+   * Swaps two values in the heap
+   *
+   * @param {int} indexA Index of the first item to be swapped
+   * @param {int} indexB Index of the second item to be swapped
+   */
+  function swap(array, indexA, indexB) {
+    var temp = array[indexA];
+    array[indexA] = array[indexB];
+    array[indexB] = temp;
+  }
+
+  /**
+   * Partitions the (sub)array into values less than and greater
+   * than the pivot value
+   *
+   * @param {Array} array The target array
+   * @param {int} pivot The index of the pivot
+   * @param {int} left The index of the leftmost element
+   * @param {int} left The index of the rightmost element
+   */
+  function partition(array, pivot, left, right) {
+
+    var storeIndex = left,
+        pivotValue = array[pivot];
+
+    // put the pivot on the right
+    swap(array, pivot, right);
+
+    // go through the rest
+    for(var v = left; v < right; v++) {
+
+      // if the value is less than the pivot's
+      // value put it to the left of the pivot
+      // point and move the pivot point along one
+      if(array[v] < pivotValue) {
+        swap(array, v, storeIndex);
+        storeIndex++;
+      }
+    }
+
+    // finally put the pivot in the correct place
+    swap(array, right, storeIndex);
+
+    return storeIndex;
+  }
+
+  /**
+   * Sorts the (sub-)array
+   *
+   * @param {Array} array The target array
+   * @param {int} left The index of the leftmost element, defaults 0
+   * @param {int} left The index of the rightmost element,
+   defaults array.length-1
+   */
+  function sort(array, left, right) {
+
+    var pivot = null;
+
+    if(typeof left !== 'number') {
+      left = 0;
+    }
+
+    if(typeof right !== 'number') {
+      right = array.length - 1;
+    }
+
+    // effectively set our base
+    // case here. When left == right
+    // we'll stop
+    if(left < right) {
+
+      // pick a pivot between left and right
+      // and update it once we've partitioned
+      // the array to values < than or > than
+      // the pivot value
+      pivot     = left + Math.ceil((right - left) * 0.5);
+      newPivot  = partition(array, pivot, left, right);
+
+      // recursively sort to the left and right
+      sort(array, left, newPivot - 1);
+      sort(array, newPivot + 1, right);
+    }
+
+  }
+
+sort(this)
 }
